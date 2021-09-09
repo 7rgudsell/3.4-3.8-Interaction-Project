c62dad5f2006a72ba6572f18c34c71402d9e52bc
7rgudsell <86265995+7rgudsell@users.noreply.github.com>
2021-09-10 09:29:03 +1200

    Delete Main Design 08.12.21 directory

--- Main Design 08.12.21/adminnav.php
+++
@@ -1,9 +0,0 @@
-<nav>
-	<ul>
-		<li><h3><a href="home.php">Home</a></h3></li>
-		<li><h3><a href="alltrackstitle.php">Query1</a></h3></li>
-		<li><h3><a href="alltracksgenre.php">Query2</a></h3></li>
-		<li><h3><a href="login.php">Login</a></h3></li>
-		<li><h3><a href="admin.php">Admin</a></h3></li>
-	</ul>
-</nav>
